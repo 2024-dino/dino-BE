c50c153cc6ff5dc9704dd08d0158e44396a43d61
HyoBN <qls0786@khu.ac.kr>
2024-09-18 15:22:31 +0900

    feat: Api 명세 추가

--- src/main/java/khu/dino/question/presentation/QuestionApi.java
+++ src/main/java/khu/dino/question/presentation/QuestionApi.java
@@ -1,4 +1,23 @@
 package khu.dino.question.presentation;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import khu.dino.question.business.QuestionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK 성공"),
+        @ApiResponse(responseCode = "500", description = "서버 에러, 백앤드 개발자에게 알려주세요."),
+})
+@Tag(name = "Question 관련 API 목록", description = "Question 관련 API 목록입니다.")
+@RequestMapping("/api/v1/question")
 public class QuestionApi {
+    private final QuestionService questionService;
 }
