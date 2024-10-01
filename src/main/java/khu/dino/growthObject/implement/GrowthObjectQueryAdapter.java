4d9c32c00c20fdf53bb9bdd63465b02ee1b6fae2
Martin0o0 <8546088@naver.com>
2024-10-01 22:04:40 +0900

    feat: 중복 순환 참조 해결

--- src/main/java/khu/dino/growthObject/implement/GrowthObjectQueryAdapter.java
+++ src/main/java/khu/dino/growthObject/implement/GrowthObjectQueryAdapter.java
@@ -2,14 +2,14 @@ package khu.dino.growthObject.implement;
 
 
 import khu.dino.common.annotation.Adapter;
-import khu.dino.growthObject.persistence.GrowthObject;
+import khu.dino.growthObject.persistence.repository.GrowthObjectRepository;
 import lombok.RequiredArgsConstructor;
 
 @Adapter
 @RequiredArgsConstructor
 public class GrowthObjectQueryAdapter {
 
-    private final GrowthObjectQueryAdapter growthObjectQueryAdapter;
+    private final GrowthObjectRepository growthObjectRepository;
 
 
 }
